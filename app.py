9537d1007f238755241088444448430cb1f92df8
featherbear <andrew.j.wong@outlook.com>
2018-08-17 13:53:16 +1000

    dump tbh forgot what this lib does

--- app.py
+++ app.py
@@ -1,85 +1,39 @@
 import ElvantoAPIExtensions
+import propresenter.files as propresenter
 
-class settings:
-    import configparser
-    import os
-    import sys
+if __name__ == "__main__":
+    class settings:
+        import configparser
+        import os
+        import sys
 
-    _configFilePath = "config.ini"
-    _config = configparser.ConfigParser()
+        _configFilePath = "config.ini"
+        _config = configparser.ConfigParser()
 
-    if not os.path.exists(_configFilePath):
-        _config.add_section("config")
-        _config.set("config","api_key", "")
-        with open(_configFilePath, "w") as _configFile:
-            _config.write(_configFile)
-        print("Created '%s'. Please enter in your API key to continue. Exiting" % _configFilePath)
-        sys.exit()
-    elif not os.path.isfile(_configFilePath):
-        raise Exception(_configFilePath + " is not a file! Aborting")
-    _config.read(_configFilePath)
-    api_key = _config.get("config","api_key")
+        if not os.path.exists(_configFilePath):
+            _config.add_section("config")
+            _config.set("config", "api_key", "")
+            with open(_configFilePath, "w") as _configFile:
+                _config.write(_configFile)
+            print("Created '%s'. Please enter in your API key to continue. Exiting" % _configFilePath)
+            sys.exit()
+        elif not os.path.isfile(_configFilePath):
+            raise Exception(_configFilePath + " is not a file! Aborting")
+        _config.read(_configFilePath)
+        api_key = _config.get("config", "api_key")
+    api = ElvantoAPIExtensions.ElvantoAPI.Connection(APIKey=settings.api_key)
 
-api = ElvantoAPIExtensions.ElvantoAPI.Connection(APIKey = settings.api_key)
+def connect(apiKey):
+    global api
+    api = ElvantoAPIExtensions.ElvantoAPI.Connection(APIKey=apiKey)
 
-import datetime
-date_today = datetime.date.today()
-date_service = date_today + datetime.timedelta( (6 - date_today.weekday()) % 7 )
+# TODO check for valid auth
 
-"""
-API Request :: services/getAll
-start | YYYY-MM-DD
-end   | YYYY-MM-DD
-page_size | int | minimum page size is 10
-"""
-services = api._Post("services/getAll", page_size = 10, start = str(date_service-datetime.timedelta(1)), end = str(date_service+datetime.timedelta(1)), fields = ["plans", "volunteers", "songs"])
-"""
-songs | only useful if songs are attached
-can extract songs from plans item
-"""
+# Elvanto API access: read only
+# ProPresenter access: read write
+# connect("B9ZUjAhyquWEqtbPVfDmS8amltDTItnzV")
+print('print(api.servicesUpcoming(locationName="Kingsgrove"))')
+print(api.servicesUpcoming(locationName="Kingsgrove"))
+print('print(propresenter.playlist.children)')
+print(propresenter.playlist.children)
 
-if services["status"] == "fail":
-    raise Exception("Could not retrieve any details for the upcoming Sunday service")
-services = services["services"]["service"]
-
-for service in services:
-    print("ID", service["id"])
-    print("Name", service["name"])
-    print("Date", service["date"])
-    print("Type", service["service_type"]["id"], service["service_type"]["name"])
-    print("Location", service["location"]["id"], service["location"]["name"])
-
-    print("Songs", service["songs"])
-
-    roles = service["volunteers"]["plan"][0]["positions"]["position"]
-    for role in roles:
-        """
-        department_name
-        sub_department_name
-        position_name
-        volunteers["volunteer"] {
-          "person" | (firstname || preferred_name), lastname
-          "status" | str
-        }
-        """
-
-    plan = service["plans"]["plan"][0]
-    planItems = plan["items"]["item"]
-    for item in planItems:
-        """
-        id
-        heading
-        duration
-        title
-        song
-        description
-        """
-        # Item is a header if (item["heading"] == 1)
-        pass
-
-    print()
-
-    # service_length; service_length_formatted
-    # total_length; total_length_formatted ????
-
-    # id, name, date, service_type[id,name], location[id,name]
